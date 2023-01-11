b362f2b79d2a639bf95c6bb5dad95663f822abf2
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-01-11 23:08:14 +0800

    chore:升级版本依赖

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,9 @@
     <div class="notification">
       姓名：{{ userInfo.userName }}
       性别：{{ userInfo.sex }}
-      <el-button type="primary">Default</el-button>
+      <el-button type="primary" class="ml-4">
+        退出
+      </el-button>
     </div>
     <NuxtWelcome/>
   </div>
@@ -18,6 +20,6 @@ const userInfo = useUserStore().userInfo
 
 <style scoped lang="scss">
 .notification {
-  @apply text-white text-center bg-emerald-400 py-1 fixed w-full;
+  @apply text-white bg-emerald-400 py-1 fixed w-full flex items-center justify-center;
 }
 </style>
