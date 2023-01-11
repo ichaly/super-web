499fa443769a05655a9648b43d9a42f5bf154ba2
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-01-11 15:40:55 +0800

    feat:集成pinia

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,15 @@
 <template>
+  <div class="text-white text-center bg-lime-500 py-1 fixed w-full">
+    姓名：{{ userInfo.userName }}
+    性别：{{ userInfo.sex }}
+  </div>
   <NuxtWelcome/>
 </template>
 
 <script setup lang="ts">
+import {useUserStore} from '@/stores/useUserStore'
+
+const userInfo = useUserStore().userInfo
 </script>
 
 <style scoped>
