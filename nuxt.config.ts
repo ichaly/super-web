44cca6022017eba4add3aec8e09c94cf45c55cf1
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-01-11 15:52:44 +0800

    feat:集成scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src/',
-  css: ["~/assets/css/tailwind.css"],
+  css: ["~/assets/styles/tailwind.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/default.scss";'
+        }
+      }
+    }
   },
   modules: [
     '@pinia/nuxt',
